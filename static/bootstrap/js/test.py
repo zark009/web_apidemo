474d705d10323ed8f2d87acc3c3c726b2920869b
zark009 <32184227+zark009@users.noreply.github.com>
2018-08-27 17:30:03 +0800

    Delete test.py

--- static/bootstrap/js/test.py
+++
@@ -1,7 +0,0 @@
-
-str="Content-Type: application/x-www-form-urlencodedContent-Length: 332Host: api.zallsoon.comConnection: Keep-AliveAccept-Encoding: gzipUser-Agent: okhttp/3.6.0token: 4329bbd57d6f94d8b70a06164a219e11zoken: b97be287e7953d797116124ba5368748
-signature: a3bf0bc23c5c9d6b024aff111191b29d
-"
-num = list(str)
-
-print (num)
